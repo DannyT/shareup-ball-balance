bdd45e6b43063db6f429195041bc539d949513b0
DannyT <dan@moov2.com>
2018-05-16 21:24:13 +0100

    move ball from browser orientation input

--- src/GameScene.js
+++ src/GameScene.js
@@ -13,14 +13,24 @@ class GameScene extends Phaser.Scene {
     // draw a ball
     this.ballGraphics = this.make.graphics();
     this.ballGraphics.fillStyle(0x000000, 1.0);
-    this.ballGraphics.fillCircle(100, 100, 100);
+    this.ballGraphics.fillCircle(50, 50, 50);
 
-    this.ballGraphics.generateTexture('ballGraphics', 200, 200);
+    this.ballGraphics.generateTexture('ballGraphics', 100, 100);
     this.ball = this.physics.add.image(100, 100, 'ballGraphics');
 
-    this.ball.setVelocity(100, 200);
+    
     this.ball.setBounce(1, 1);
     this.ball.setCollideWorldBounds(true);
+
+    if (window.DeviceOrientationEvent) {
+		
+      window.addEventListener("deviceorientation", (event) => {
+        this.ball.setVelocity(event.gamma*25, event.beta*25);
+      });
+    }
+    else {
+      console.log("Sorry, your browser doesn't support Device Orientation");
+    };
   }
 
   update(time, delta) {
